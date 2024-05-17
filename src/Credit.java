8b9dfbe392d177f050fdaceab87f7b88bcf1f1c5
fators <fators@bergen.org>
2024-05-17 13:56:29 -0400

    Added findDigits method with main method.

--- src/Credit.java
+++ src/Credit.java
@@ -1,2 +1,14 @@
 public class Credit {
+    public static int findDigits(long num){
+        int count =0;
+        while( num>0){
+
+            int digit=(int)num%10;
+            count++;
+            num=num/10;
+
+        }
+
+        return count;
+    }
 }
